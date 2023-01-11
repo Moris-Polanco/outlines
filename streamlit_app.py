90f5d1f4a548eb784f70a87040e46ac97b64a3ef
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-11 08:48:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,26 +5,20 @@ import os
 # Autenticación de OpenAI (oculta la clave en una variable de entorno)
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-# Intialize GPT-3 model 
-tokenizer = openai.AutoTokenizer.from_pretrained('text-davinci-003') 
-model = openai.AutoModelWithLMHead.from_pretrained('text-davinci-003') 
+st.title("GPT-3 Essay Outline Generator")
 
-# Create the Streamlit app 
-st.title("GPT-3 Outline Generator") 
-prompt = st.text_input('Enter your essay topic:') 
+st.header("Enter Your Essay Title Here")
 
-# Generate the essay outline 
-if st.button('Generate Outline'): 
-    with st.spinner('Generating Outline...'): 
-        generated_outline = tokenizer.generate(
-            prompt,
-            num_return_sequences=3,
-            max_length=150,
-            return_tensors='pt'
-        ) 
+title = st.text_input("")
 
-# Display the essay outline 
-for i in range(generated_outline.shape[0]): 
-    outline = tokenizer.decode(generated_outline[i], skip_special_tokens=True) 
-    st.markdown(f"#### Outline {i+1}") 
-    st.success(outline)
+st.write("Generating Essay Outline...")
+
+response = api.Engine(engine="text-davinci-003").completion(
+    prompt=title,
+    max_tokens=1000,
+    temperature=0.7,
+    top_p=0.9,
+    frequency_penalty=0.2
+)
+
+st.write(response['choices'][0]['text'])
