4994688413d6039b17cd5e9971e6a3f4498c8548
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-11 08:54:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,21 @@ import os
 # Autenticación de OpenAI (oculta la clave en una variable de entorno)
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-st.title("GPT-3 Essay Outline Generator")
+# Set page title
+st.title('GPT-3 Essay Outliner')
 
-st.header("Enter Your Essay Title Here")
+# Get essay title from user
+title = st.text_input('Enter the title of your essay:')
 
-title = st.text_input("")
-
-st.write("Generating Essay Outline...")
-
-response = completions = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
+# Generate essay outline using OpenAI GPT-3
+prompt = f"Outline: {title}"
+response = openai.Completion.create(
+    engine="davinci",
+    prompt=prompt,
+    temperature=0.7,
+    max_tokens=50,
+    top_p=0.9
 )
 
+# Show essay outline
 st.write(response['choices'][0]['text'])
