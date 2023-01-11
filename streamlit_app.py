b0d01582ee486450f8a2d2b716155ca723629234
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-11 08:28:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.markdown("Esta aplicación te ayudará a crear outlines de artículos usando 
 input_text = st.text_input("Introduzca el texto de entrada")
 
 # Usar OpenAI GPT-3 Text-Davinci-003 para generar el outline
-outline = openai.Completion.create(engine="davinci-003", prompt=input_text, max_tokens=100)
+outline = openai.Completion.create(engine="text-davinci-003", prompt=input_text, max_tokens=1000)
 
 # Mostrar el outline generado
 st.markdown("El outline generado es el siguiente:")
