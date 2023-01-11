63389c80dc7d6159924bbb70110311648c97ec13
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-11 08:40:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,22 @@ import os
 # Autenticación de OpenAI (oculta la clave en una variable de entorno)
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-# Crear una página de bienvenida
-st.title("GENERADOR DE ESQUEMAS")
-st.markdown("Esta aplicación te ayudará a crear outlines de artículos usando OpenAI GPT-3 Text-Davinci-003.")
+# Create the Streamlit app 
+st.title("GPT-3 Outline Generator") 
+prompt = st.text_input('Enter your essay topic:') 
 
-# Obtener el texto de entrada del usuario
-input_text = st.text_input("Introduzca el texto de entrada")
+# Generate the essay outline 
+if st.button('Generate Outline'): 
+    with st.spinner('Generating Outline...'): 
+        generated_outline = tokenizer.generate(
+            prompt,
+            num_return_sequences=3,
+            max_length=150,
+            return_tensors='pt'
+        ) 
 
-# Usar OpenAI GPT-3 Text-Davinci-003 para generar el outline
-outline = openai.Completion.create(engine="text-davinci-003", prompt=input_text, max_tokens=1000)
-
-# Mostrar el outline generado
-st.markdown("El outline generado es el siguiente:")
-st.write(outline["choices"][0]["text"])
+# Display the essay outline 
+for i in range(generated_outline.shape[0]): 
+    outline = tokenizer.decode(generated_outline[i], skip_special_tokens=True) 
+    st.markdown(f"#### Outline {i+1}") 
+    st.success(outline).
